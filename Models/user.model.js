253ec8637998ef71322c9b8e689005c62463c45d
charuka herath <charuka09@gmail.com>
2018-08-17 21:40:33 +0530

    email validation

--- Models/user.model.js
+++ Models/user.model.js
@@ -1,17 +1,38 @@
 const mongoose =  require('mongoose');
+const bcrypt = require('bcryptjs');
 
 var userSchema =  new mongoose.Schema({
     fullName: {
-        type:String
+        type:String,
+        required: 'Full name can\'t be empty'
     },
     email: {
-        type:String
+        type:String,
+        required: 'email can\'t be empty'
     },
     password: {
-        type:String
+        type:String,
+        required: 'Password can\'t be empty'
     },
     //used for encryption and decryptyion
     saltSecret: String
+});
+
+//custom validation
+userSchema.path('email').validate((val) => {
+    emailval = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return emailval.test(val);
+}, 'Invalid email');
+
+//events
+userSchema.pre('save', function(next) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err,hash) => {
+            this.password = hash;
+            this.saltSecret = salt;
+            next();
+        });
+    });
 });
 
 //register user schema objecct in mongoose
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,16 @@
 //fuction to user registration and sign up
-module.exports.register = (req,res,next) =>{
-    console.log('inside register func');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('user');
+
+module.exports.register = (req,res,next) => {
+    var user =  new User();
+    user.fullName = req.body.fullName;
+    user.email = req.body.email;
+    user.password = req.body.password;
+    user.save((err,doc) => {
+        if(!err){
+            res.send(doc);
+        }
+    });
 }
